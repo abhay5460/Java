f3180a1339734a5e917aab7bbf4dd4c4638177de
Abhay Gajjar <gajjarabhay82@gmail.com>
2025-01-07 18:13:46 +0530

    Add files via upload

---
+++ Module-6/Practical/Introduction_to_java/New_Demo.java
@@ -0,0 +1,11 @@
+package com.technical1;
+
+//Write a simple "Hello World" Java program.
+
+public class New_Demo
+{
+    public static void main(String[] args) 
+    {
+    	System.out.println("Hello World");
+    }
+}
---
+++ Module-6/Practical/Introduction_to_java/New_Demo2.java
@@ -0,0 +1,22 @@
+package com.technical1;
+
+//Compile and run the program using command-line tools (javac, java).
+
+public class New_Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+	
+	
+	// Open Command Prompt
+	// Example = cd path/to/your/directory
+	
+	// Compile the java program
+	// Example = javac HelloWorld.java
+
+	// Run the Java Program
+	// Output = Hello World
+
+}
